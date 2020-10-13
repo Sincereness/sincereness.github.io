a2cb28907224c7c0fc0f491cdcabff1a3cec082a
84h3p <vano0448@gmail.com>
2020-10-14 02:40:16 +0600

    new file:   fonts/main.otf 	new file:   fonts/main.ttf 	new file:   "fonts/\320\232\320\241391 \320\224.\320\267. \320\237\320\240\342\204\2267 \320\222\321\213\320\277\320\276\320\273\320\275\320\265\320\275\320\270\320\265 \321\207\320\265\321\200\321\202\320\265\320\266\320\260 \320\234\320\276\320\264\320\265\320\273\321\214.docx" 	new file:   index.html 	new file:   scripts/script.js 	new file:   style.css

---
+++ scripts/script.js
@@ -0,0 +1,36 @@
+const result = document.getElementById("loveresult");
+
+const content = document.getElementById("contentPage");
+
+const phrases = ['За твои приятные духи', 'За твои тёплыё обьятия',
+ 'За твои бип-бип', 'За твои куси перед сном', 'За твою нужную помощь',
+  'За твои милые глазки', 'За твой приятный голос', 'За то, что ты всегда рядом',
+  'За твой забавный смех', 'За твои согревающие ручки']
+
+let phrasesCopy = phrases.slice(0)
+
+function lovere(e) {
+
+
+
+  const index = Math.floor(Math.random()*phrasesCopy.length)
+  const phrase = phrasesCopy[index];
+
+ content.innerHTML = phrasesCopy[index];
+
+ document.getElementById("btn").value= "А за что ещё?";
+
+ phrasesCopy.splice(index, 1)
+
+ if (phrasesCopy.length === 0) {
+      phrasesCopy = phrases.slice(0)
+      phrasesCopy.splice(phrases.indexOf(phrase), 1)
+  }
+console.log(phrasesCopy)
+
+
+
+}
+
+result-btn.addEventListener("click", lovere)
+btnresult.addEventListener('submit', (event) => event.preventDefault())
